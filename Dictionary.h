7d12f5b4a73c2d330ebd7ffcc6ced18cc158446f
Kimberly Lindholm <klindholm7@gmail.com>
2017-04-04 18:32:50 -0600

    add skeleton of method implementations

--- Dictionary.h
+++ Dictionary.h
@@ -26,5 +26,48 @@ public:
     int getCount() const { return count; }
 };
 
+const int DEFAULT_DICTIONARY_SIZE = 10;
+
+//set default size to default size lol
+template <typename X, typename Y>
+Dictionary<X,Y>::Dictionary() {
+
+}
+
+//set default size to the size parameter
+template <typename X, typename Y>
+Dictionary<X,Y>::Dictionary(int size) {
+
+}
+
+//copy constructor
+template <typename X, typename Y>
+Dictionary<X,Y>::Dictionary(const Dictionary &) {}
+
+template <typename X, typename Y>
+void Dictionary<X,Y>::add(X key, Y value) {
+
+}
+
+template <typename X, typename Y>
+KeyValue<X, Y> Dictionary<X,Y>::getByKey(X key) {
+
+}
+
+template <typename X, typename Y>
+KeyValue<X, Y> Dictionary<X,Y>::getByIndex(int index) {
+
+}
+
+template <typename X, typename Y>
+void Dictionary<X,Y>::removeByKey(X key) {
+
+}
+
+template <typename X, typename Y>
+void Dictionary<X,Y>::removeByIndex(int index) {
+
+}
+
 
 #endif //GENERICDICTIONARY_DICTIONARY_H

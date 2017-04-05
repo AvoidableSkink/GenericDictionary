feb068a941e080686efeac21374f0e28c8f80311
Kimberly Lindholm <klindholm7@gmail.com>
2017-04-04 20:40:12 -0600

    add the testing files

---
+++ Testing/KeyValueTester.h
@@ -0,0 +1,14 @@
+//
+// Created by klind on 4/4/2017.
+//
+
+#ifndef GENERICDICTIONARY_KEYVALUETESTER_H
+#define GENERICDICTIONARY_KEYVALUETESTER_H
+
+
+class KeyValueTester {
+
+};
+
+
+#endif //GENERICDICTIONARY_KEYVALUETESTER_H
---
+++ Testing/testMain.cpp
@@ -0,0 +1,11 @@
+//
+// Created by klind on 4/4/2017.
+//
+
+#include <iostream>
+#include "DictionaryTester.h"
+#include "KeyValueTester.h"
+
+int main() {
+    std::cout << "Testing time" << std::endl;
+}

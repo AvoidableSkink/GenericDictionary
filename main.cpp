791096e1cf72e18a665147353336c250e2ff71df
Kimberly Lindholm <klindholm7@gmail.com>
2017-04-05 19:25:38 -0600

    implement very simple main.cpp to show a use of the dictionary class

--- main.cpp
+++ main.cpp
@@ -4,4 +4,20 @@
 
 int main() {
 
+    Dictionary<std::string, std::string> myShows;
+    myShows.add("Hwarang", "Not Finished");
+    myShows.add("Strong Woman", "Still Airing");
+    myShows.add("City Hunter", "Completed -- many times");
+    myShows.add("House", "Complete");
+    myShows.add("The Heirs", "Complete");
+
+    myShows.removeByKey("House");
+
+    for (int i = 0; i < myShows.getCount(); ++i) {
+        KeyValue<std::string, std::string> myKeyVal = myShows.getByIndex(i);
+        std::cout << "Show: " << myKeyVal.getKey() << "   Status: " << myKeyVal.getValue() << std::endl;
+    }
+
+    KeyValue<std::string, std::string> myKeyVal = myShows.getByKey("City Hunter");
+    std::cout << "My favorite show is " << myKeyVal.getKey() << " and its current status is " << myKeyVal.getValue() << std::endl;
 }
